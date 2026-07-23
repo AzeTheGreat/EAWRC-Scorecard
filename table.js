db26e94771aba99783abf7039060b11ec5b42304
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2026-07-22 21:34:34 -0700

    Refac: File structure

--- table.js
+++ table.js
@@ -121,27 +121,6 @@ function getChildren(entry) {
   return lvl.getChildIds(id).map(cid => ({ lvl: lvl.childLevel, id: cid }));
 }
 
-function getTotals(xEntry, yEntry) {
-  const entries = window.apiData?.entries;
-  if (!entries) return "";
-
-  const filters = {};
-  if (xEntry) filters[xEntry.lvl.levelID] = xEntry.id;
-  if (yEntry) filters[yEntry.lvl.levelID] = yEntry.id;
-
-  const matching = entries.filter(
-    e =>
-      (!filters.class || e.vehicleClassId === filters.class) &&
-      (!filters.drivetrain || (window.ClassIdsByDrivetrain[filters.drivetrain] || []).includes(e.vehicleClassId)) &&
-      (!filters.location || e.locationId === filters.location) &&
-      (!filters.surface || (window.LocIdsBySurface[filters.surface] || []).includes(e.locationId)) &&
-      (!filters.stage || e.routeId === filters.stage)
-  );
-  if (!matching.length) return "";
-  const sum = matching.reduce((s, e) => s + (e.rank / e.totalEntries) * 100, 0);
-  return Math.round(sum / matching.length);
-}
-
 
 // Get HTML
 function getChunk(className, groupCell, cells) {
